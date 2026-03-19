7533dca3a9b6bf1412ceeff6b7e46696a07385aa
Bruno Tenorio <tenoriobn@gmail.com>
2026-03-19 15:15:43 -0300

    feat: :sparkles: 02/06 - Customizando o Drawer

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,7 +4,22 @@ import { Drawer } from "expo-router/drawer";
 export default function Layout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer />
+      <Drawer
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#021123",
+          },
+          headerTintColor: "#fff",
+          drawerStyle: {
+            backgroundColor: "#021123",
+          },
+          drawerLabelStyle: {
+            color: "#FFF",
+          },
+        }}
+      >
+        <Drawer.Screen name="add-task/index" options={{ headerShown: true }} />
+      </Drawer>
     </GestureHandlerRootView>
   );
 }
---
+++ app/add-task/index.jsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function AddTask() {
+  return (
+    <SafeAreaView>
+      <Text>Adicionar uma tarefa</Text>
+    </SafeAreaView>
+  );
+}
