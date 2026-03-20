3b342727a6effd4fc41ebf17fe3a449a9ed8a569
Bruno Tenorio <tenoriobn@gmail.com>
2026-03-20 12:09:16 -0300

    feat: :sparkles: 05/06 - Listando tarefas do contexto

--- app/tasks/index.jsx
+++ app/tasks/index.jsx
@@ -4,15 +4,25 @@ import TaskItem from "../../components/TaskItem";
 import { FokusButton } from "../../components/FokusButton";
 import { IconPlus } from "../../components/Icons";
 import { router } from "expo-router";
+import useTaskContent from "../../components/context/useTaskContent";
 
 export default function Tasks() {
+  const { tasks } = useTaskContent();
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.wrapper}>
         <Text style={styles.text}>Lista de tarefas:</Text>
         <View style={styles.inner}>
-          <TaskItem completed={true} text="Estudar React" />
-          <TaskItem text="Estudar React Native" />
+          {tasks.map((t) => {
+            return (
+              <TaskItem
+                key={t.id}
+                completed={t.completed}
+                text={t.description}
+              />
+            );
+          })}
         </View>
 
         <FokusButton
