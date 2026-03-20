97fc8e957fef7bff3381a2eaa16b1e31a9877310
Bruno Tenorio <tenoriobn@gmail.com>
2026-03-20 11:05:28 -0300

    feat: :sparkles: 04/02 - Implementando o formulário

--- app/add-task/index.jsx
+++ app/add-task/index.jsx
@@ -1,10 +1,20 @@
-import { Text } from "react-native";
+import { Pressable, Text, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { IconSave } from "../../components/Icons";
 
 export default function AddTask() {
   return (
     <SafeAreaView>
       <Text>Adicionar uma tarefa</Text>
+
+      <View>
+        <Text>Em que você está trabalhando?</Text>
+        <TextInput />
+        <Pressable>
+          <IconSave />
+          <Text>Salvar</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 }
