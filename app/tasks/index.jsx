0551a63a7d2a398f007fb017de9ee59ed5067a0a
Bruno Tenorio <tenoriobn@gmail.com>
2026-03-20 12:22:05 -0300

    feat: :sparkles: 05/07 - Scroll de tarefas

--- app/tasks/index.jsx
+++ app/tasks/index.jsx
@@ -1,10 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import TaskItem from "../../components/TaskItem";
 import { FokusButton } from "../../components/FokusButton";
 import { IconPlus } from "../../components/Icons";
 import { router } from "expo-router";
 import useTaskContent from "../../components/context/useTaskContent";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Tasks() {
   const { tasks } = useTaskContent();
@@ -12,25 +12,33 @@ export default function Tasks() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.wrapper}>
-        <Text style={styles.text}>Lista de tarefas:</Text>
         <View style={styles.inner}>
-          {tasks.map((t) => {
-            return (
+          <FlatList
+            data={tasks}
+            renderItem={({ item }) => (
               <TaskItem
-                key={t.id}
-                completed={t.completed}
-                text={t.description}
+                completed={item.completed}
+                text={item.description}
+                key={item.id}
               />
-            );
-          })}
+            )}
+            keyExtractor={(item) => item.id}
+            ItemSeparatorComponent={() => <View style={{ height: 8 }} />}
+            ListHeaderComponent={
+              <Text style={styles.text}>Lista de tarefas:</Text>
+            }
+            ListFooterComponent={
+              <View style={{ marginTop: 16 }}>
+                <FokusButton
+                  title="Adicionar nova tarefa"
+                  icon={<IconPlus outline />}
+                  outline
+                  onPress={() => router.navigate("/add-task")}
+                />
+              </View>
+            }
+          />
         </View>
-
-        <FokusButton
-          title="Adicionar nova tarefa"
-          icon={<IconPlus outline />}
-          outline
-          onPress={() => router.navigate("/add-task")}
-        />
       </View>
     </SafeAreaView>
   );
@@ -50,6 +58,7 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: "#fff",
     fontSize: 26,
+    marginBottom: 16,
   },
   inner: {
     gap: 8,
