a40e19796cbf8d86bf8b42be61d2e1f2e78e6d03
Bruno Tenorio <tenoriobn@gmail.com>
2026-03-20 11:15:53 -0300

    feat: :sparkles: 04/04 - estilizando o forms de tarefa

--- app/add-task/index.jsx
+++ app/add-task/index.jsx
@@ -1,16 +1,16 @@
-import { Pressable, Text, TextInput, View } from "react-native";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { IconSave } from "../../components/Icons";
 
 export default function AddTask() {
   return (
-    <SafeAreaView>
-      <Text>Adicionar uma tarefa</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Adicionar uma tarefa</Text>
 
-      <View>
-        <Text>Em que você está trabalhando?</Text>
-        <TextInput />
-        <Pressable>
+      <View style={styles.inner}>
+        <Text style={styles.label}>Em que você está trabalhando?</Text>
+        <TextInput numberOfLines={10} style={styles.input} />
+        <Pressable style={styles.button}>
           <IconSave />
           <Text>Salvar</Text>
         </Pressable>
@@ -18,3 +18,39 @@ export default function AddTask() {
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#021123",
+    gap: 16,
+    alignItems: "center",
+  },
+  text: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 26,
+  },
+  inner: {
+    backgroundColor: "#98A0A8",
+    width: "90%",
+    borderRadius: 8,
+    padding: 16,
+    gap: 32,
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: 600,
+  },
+  input: {
+    backgroundColor: "#fff",
+    padding: 16,
+    borderRadius: 8,
+  },
+  button: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 4,
+    justifyContent: "flex-end",
+  },
+});
